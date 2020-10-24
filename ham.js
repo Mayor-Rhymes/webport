ba40fdfe3de28dd9054343f8b2447acb13a2ac79
Mayor-Rhymes <35629340+Mayor-Rhymes@users.noreply.github.com>
2020-10-24 11:28:43 +0300

    Add files via upload

---
+++ ham.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+    var dope = document.getElementById("crew");
+
+    function show()
+    {
+
+      if(dope.style.display === "none")
+      {
+
+        
+          dope.style.display = "flex";
+
+        
+      }
+      else
+      {
+
+
+        dope.style.display = "none";
+
+      }
+
+
+
+    }
+
+
+
+    
+
+
+    
